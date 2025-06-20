f3ac8210252db5df8c6526d581c9d3a9b7c0acbd
Fasalsan <Sanfasal70@gmail.com>
2025-06-20 14:49:48 +0700

    test update

--- src/pages/ProductCardList.js
+++ src/pages/ProductCardList.js
@@ -29,6 +29,8 @@ const ProductCardList = () => {
 
     return (
         <div className="p-4">
+
+            <h1 className="text-2xl font-bold mb-4 text-red-700">សូមស្វាគមន៍​មកកាន់ផេក​សាន់សាន់</h1>
             <h1 className="text-2xl font-bold mb-4">Product List</h1>
             <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {products.map(product => (
